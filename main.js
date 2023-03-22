a846d8c00f9a8395ba9c72963f9e6882b535eb94
David Lidebrandt <david.lidebrandt@gmail.com>
2023-03-22 17:58:52 +0100

    Test

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDTiLkekA8Q65_krnZG280y60kRhuj5n-U",
-    authDomain: "maries-bootstrap.firebaseapp.com",
-    projectId: "maries-bootstrap",
-    storageBucket: "maries-bootstrap.appspot.com",
-    messagingSenderId: "1083228106927",
-    appId: "1:1083228106927:web:262625f554102cbfcda45a",
-    measurementId: "G-F71JZW76QJ"
+    apiKey: "AIzaSyBQvGgwuMTgiygpF_37uuV9A_Ls5g4dAdg",
+    authDomain: "maries-fothalsa-5df36.firebaseapp.com",
+    projectId: "maries-fothalsa-5df36",
+    storageBucket: "maries-fothalsa-5df36.appspot.com",
+    messagingSenderId: "502780488595",
+    appId: "1:502780488595:web:af4c80cdd92a8de80e9666",
+    measurementId: "G-Y8XGM2P1HC"
   };
 
 const app = initializeApp(firebaseConfig);
